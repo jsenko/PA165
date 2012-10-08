556c04d2e406849c824186f395fde57619988aa8
jsenko <jsenko@redhat.com>
2012-10-08 22:00:52 +0200

    Created project modules and sample implementation.

---
+++ app/src/main/java/cz/muni/fi/pa165/fast/AppTest.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pa165.fast;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
