a6d6229dbafbd379cb9d5327ccfa73ed50578663
misenko <kimle.michal@gmail.com>
2012-10-11 23:04:37 +0200

    Goal entity and player DAO interface. Updated design.vpp. Modified .gitignore because of Visual Paradigm files. Removed test scope from pom.xml

--- app/src/main/java/cz/muni/fi/pa165/fast/dao/PlayerDAO.java
+++ app/src/main/java/cz/muni/fi/pa165/fast/dao/PlayerDAO.java
@@ -1,6 +1,86 @@
 package cz.muni.fi.pa165.fast.dao;
 
+import cz.muni.fi.pa165.fast.model.Goal;
+import cz.muni.fi.pa165.fast.model.Match;
+import cz.muni.fi.pa165.fast.model.Player;
+import java.util.List;
+
+/**
+ *Interface of DAO object of entity Player
+ * 
+ * @author Michal Kimle
+ */
 public interface PlayerDAO
 {
-
+    /**
+     * Creates new Player in the database.
+     * 
+     * @param player
+     * @throws IllegalArgumentException if argument is null
+     */
+    public void createPlayer(Player player);
+    
+    /**
+     * Updates a Player in the database.
+     * 
+     * @param player
+     * @throws IllegalArgumentException if argument is null
+     */
+    public void updatePlayer(Player player);
+    
+    /**
+     * Removes a Player in the database.
+     * 
+     * @param player
+     * @throws IllegalArgumentException if argument is null
+     */
+    public void removePlayer(Player player);
+    
+    /**
+     * Retrieves a Player with given id from database.
+     * 
+     * @param id Id of Player. Cannot be null.
+     * @return Player with the specified id or null if it does not exist.
+     * @throws IllegalArgumentException if the argument is null.
+     */
+    public Player getPlayerById(Long id);
+    
+    /**
+     * Retrieves a Player who scored given goal.
+     * 
+     * @param goal Scored goal. Cannot be null.
+     * @return Player who scored the goal or null if it does not exist.
+     * @throws IllegalArgumentException if the argument is null.
+     */
+    public Player getPlayerByScoredGoal(Goal goal);
+    
+    /**
+     * Retrieves a Player who assisted given goal.
+     * 
+     * @param goal Assisted goal. Cannot be null.
+     * @return Player who assisted the goal or null if it does not exist.
+     * @throws IllegalArgumentException if the argument is null.
+     */
+    public Player getPlayerByAssistedGoal(Goal goal);
+    
+    /**
+     * Retrieves all Players
+     * 
+     * @return List of all Players in database or empty List if database is empty
+     */
+    public List<Player> findAllPlayers();
+    
+    /**
+     * Retrieves home Players from given match
+     * 
+     * @return List of home Players from match or empty List if match not found
+     */
+    public List<Player> findHomePlayersByMatch(Match match);
+    
+    /**
+     * Retrieves away Players from given match
+     * 
+     * @return List of away Players from match or empty List if match not found
+     */
+    public List<Player> findAwayPlayersByMatch(Match match);
 }
--- app/src/main/java/cz/muni/fi/pa165/fast/model/Goal.java
+++ app/src/main/java/cz/muni/fi/pa165/fast/model/Goal.java
@@ -1,6 +1,80 @@
 package cz.muni.fi.pa165.fast.model;
 
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ * Entity - Goal
+ * 
+ * @author Michal Kimle
+ */
+@Entity
 public class Goal
 {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date goalTime;
+    private Long scorePlayer;
+    private Long assistPlayer;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getGoalTime() {
+        return goalTime;
+    }
+
+    public void setGoalTime(Date goalTime) {
+        this.goalTime = goalTime;
+    }
+
+    public Long getScorePlayer() {
+        return scorePlayer;
+    }
+
+    public void setScorePlayer(Long scorePlayer) {
+        this.scorePlayer = scorePlayer;
+    }
+
+    public Long getAssistPlayer() {
+        return assistPlayer;
+    }
+
+    public void setAssistPlayer(Long assistPlayer) {
+        this.assistPlayer = assistPlayer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Goal other = (Goal) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Goal{" + "id=" + id + ", goalTime=" + goalTime + ", scorePlayer=" + scorePlayer + ", assistPlayer=" + assistPlayer + '}';
+    }
 }
