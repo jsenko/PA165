157107f91db9a13f3e54fe78baeb8432ae3fd1ad
Stefan Uhercik <steve.uhercik@gmail.com>
2012-10-12 22:35:31 +0200

    implementation of Match and TeamDAO

--- app/src/main/java/cz/muni/fi/pa165/fast/dao/TeamDAO.java
+++ app/src/main/java/cz/muni/fi/pa165/fast/dao/TeamDAO.java
@@ -1,6 +1,57 @@
 package cz.muni.fi.pa165.fast.dao;
 
+import cz.muni.fi.pa165.fast.model.Player;
+import cz.muni.fi.pa165.fast.model.Team;
+import java.util.Collection;
+
+/**
+ * This interface represents data access object for Team
+ * 
+ * @author Stefan
+ */
 public interface TeamDAO
 {
-
+    /**
+     * Creates new team in the database
+     * 
+     * @param team Team to be created, cannot be null.
+     */
+    public void create(Team team);
+    
+    /**
+     * Updates team in the database
+     * 
+     * @param team Team to be updated, cannot be null.
+     */
+    public void update(Team team);
+    
+    /**
+     * Deletes team from database
+     * 
+     * @param team Team to be deleted, cannot be null
+     */
+    public void delete(Team team);
+    
+    /**
+     * Finds the team by id
+     * 
+     * @param id Id of the team to be found, cannot be null
+     * @return Team with specified id.
+     */
+    public Team getById(Long id);
+    
+    /**
+     * Finds all the teams from database
+     * 
+     * @return Collection of all teams from database or empty Collection if there are no teams registered
+     */
+    public Collection<Team> findAll();
+    
+    /**
+     * Finds the team that Player plays for
+     * 
+     * @param player
+     * @return Team that Player plays for
+     */
+    public Team findByPlayer(Player player);
 }
--- app/src/main/java/cz/muni/fi/pa165/fast/model/Match.java
+++ app/src/main/java/cz/muni/fi/pa165/fast/model/Match.java
@@ -1,6 +1,98 @@
 package cz.muni.fi.pa165.fast.model;
 
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+/**
+ * Entity Match
+ * 
+ * @author Stefan
+ */
+@Entity
 public class Match
 {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Temporal(TemporalType.DATE)
+    private Date matchDate;
+    @OneToOne
+    private Team homeTeam;
+    @OneToOne
+    private Team awayTeam;
+    @OneToMany
+    private Collection<Goal> goals;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
+
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" + "id=" + id + ", matchDate=" + matchDate + ", homeTeam=" + homeTeam + ", awayTeam=" + awayTeam + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Match other = (Match) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
 }
