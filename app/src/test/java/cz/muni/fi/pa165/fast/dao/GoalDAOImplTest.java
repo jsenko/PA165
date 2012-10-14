cb34bb6346949284e74cad51293a4924f77c65f6
Stefan Uhercik <steve.uhercik@gmail.com>
2012-10-14 23:20:45 +0200

    Modified GoalDAOImplTest and PlayerDAOImplTest

--- app/src/test/java/cz/muni/fi/pa165/fast/dao/GoalDAOImplTest.java
+++ app/src/test/java/cz/muni/fi/pa165/fast/dao/GoalDAOImplTest.java
@@ -6,10 +6,11 @@ package cz.muni.fi.pa165.fast.dao;
 
 import cz.muni.fi.pa165.fast.dao.impl.GoalDAOImpl;
 import cz.muni.fi.pa165.fast.model.Goal;
-import cz.muni.fi.pa165.fast.model.Match;
 import cz.muni.fi.pa165.fast.model.Player;
-import java.util.Collection;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -25,33 +26,32 @@ import static org.junit.Assert.*;
  * @author Stefan
  */
 public class GoalDAOImplTest {
-    
+
     private GoalDAOImpl gdaoi;
     private EntityManagerFactory emf;
-    
+
     public GoalDAOImplTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         emf = Persistence.createEntityManagerFactory("TestPU");
-        gdaoi= new GoalDAOImpl();
+        gdaoi = new GoalDAOImpl();
         gdaoi.setEntityManagerFactory(emf);
     }
-    
+
     @After
     public void tearDown() {
         emf.close();
     }
-
 
     /**
      * Test of create method, of class GoalDAOImpl.
@@ -69,24 +69,24 @@ public class GoalDAOImplTest {
     @Test
     public void testUpdate() {
         Goal goal = new Goal();
-        
+
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         em.persist(goal);
         em.getTransaction().commit();
         em.close();
-        
+
         long milis = 895465778;
         goal.setGoalTime(new Date(milis));
-        
+
         gdaoi.update(goal);
-       
+
         EntityManager em2 = emf.createEntityManager();
         Goal goalFromDB = em2.find(Goal.class, goal.getId());
         em2.close();
-        
-        assertEquals(milis,goal.getGoalTime().getTime());
-        
+
+        assertEquals(milis, goal.getGoalTime().getTime());
+
     }
 
     /**
@@ -100,15 +100,44 @@ public class GoalDAOImplTest {
         em.persist(goal);
         em.getTransaction().commit();
         em.close();
-        
+
         Long goalId = goal.getId();
-        
+
         gdaoi.delete(goal);
-        
+
         EntityManager em2 = emf.createEntityManager();
-        Goal goalFromDB = em2.find(Goal.class,goalId);
+        Goal goalFromDB = em2.find(Goal.class, goalId);
         assertNull(goalFromDB);
     }
 
-   
+    @Test
+    public void testFindAll() {
+        Random r = new Random();
+        EntityManager em = emf.createEntityManager();
+
+        List<Goal> list = new LinkedList<Goal>();
+        em.getTransaction().begin();
+
+        Goal g = new Goal();
+
+        g.setGoalTime(new Date(r.nextInt(15000)));
+        list.add(g);
+        em.persist(g);
+
+        Goal g2 = new Goal();
+        g2.setGoalTime(new Date(r.nextInt(15000)));
+        list.add(g2);
+        em.persist(g2);
+
+        em.getTransaction().commit();
+       
+        List<Goal> goalsFromDB = (List<Goal>) gdaoi.findAll();
+
+        assertEquals(list, goalsFromDB);
+        em.close();
+
+    }
+    
+    
+    
 }
--- app/src/test/java/cz/muni/fi/pa165/fast/dao/PlayerDAOImplTest.java
+++ app/src/test/java/cz/muni/fi/pa165/fast/dao/PlayerDAOImplTest.java
@@ -5,15 +5,13 @@
 package cz.muni.fi.pa165.fast.dao;
 
 
-import static org.junit.Assert.fail;
 
+import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -22,6 +20,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cz.muni.fi.pa165.fast.dao.impl.PlayerDAOImpl;
+import cz.muni.fi.pa165.fast.dao.impl.TeamDAOImpl;
 import cz.muni.fi.pa165.fast.model.Player;
 import cz.muni.fi.pa165.fast.model.Team;
 
@@ -64,33 +63,21 @@ public class PlayerDAOImplTest {
         
         try{
             playerDAO.create(player);
-            Assert.fail("Null pointer exception shoud be thrown whne null player creating.");
+            fail("Null pointer exception shoud be thrown whne null player creating.");
         }catch(IllegalArgumentException ex)
         {
             //OK
         }
        
         player = new Player();
+        player.setName("Mike");
         
         try{
             playerDAO.create(player);
         }catch(Exception ex)
         {
-            Assert.fail("Error when player is created.");
+            fail("Error when player is created.");
         } 
-        
-        /* TODO: remove, this is actually ok
-        Player player2 = new Player();
-        player2.setId(null);
-        try
-        {
-            playerDAO.create(player2);
-            Assert.fail("Error when player with null id is created");
-        }catch(IllegalArgumentException ex)
-        {
-            //OK
-        }
-        */
     }
     
     @Test
@@ -101,7 +88,7 @@ public class PlayerDAOImplTest {
         
         try{
             playerDAO.update(player);
-            Assert.fail("Null pointer exception shoud be thrown whne null player updating.");
+            fail("Null pointer exception shoud be thrown whne null player updating.");
         }catch(IllegalArgumentException ex)
         {
             //OK
@@ -115,7 +102,7 @@ public class PlayerDAOImplTest {
             playerDAO.update(player);
         }catch(Exception ex)
         {
-            Assert.fail("Error when player is updated.");
+            fail("Error when player is updated.");
         }         
     }
     
@@ -127,7 +114,7 @@ public class PlayerDAOImplTest {
         
         try{
             playerDAO.delete(player);
-            Assert.fail("Null pointer exception shoud be thrown whne null player creating.");
+            fail("Null pointer exception shoud be thrown whne null player creating.");
         }catch(IllegalArgumentException ex)
         {
             //OK
@@ -141,7 +128,7 @@ public class PlayerDAOImplTest {
             
         }catch(Exception ex)
         {
-            Assert.fail("Error when player is deleted.");
+            fail("Error when player is deleted.");
         }         
     }
     
@@ -149,43 +136,23 @@ public class PlayerDAOImplTest {
     public void findAlltest()
     {
         Player player1 = new Player();
+        player1.setName("Mike");
         Player player2 = new Player();
+        player2.setName("Felix");
         
         playerDAO.create(player1);
         playerDAO.create(player2);
         
-        Collection<Player> players = playerDAO.findAll();
+        Collection<Player> allPlayers = playerDAO.findAll();
         
-        Assert.assertEquals(2, players.size());
+        Assert.assertEquals(2, allPlayers.size());
         
-        Assert.assertTrue(players.contains(player1));
-        Assert.assertTrue(players.contains(player2));
-        
-    }
-    
-    @Test
-    public void findPlayersByTeam()
-    {
-        Team team = new Team();
-        Player player1 = new Player();
-        Player player2 = new Player();
-        
-        team.setPlayers(new ArrayList<Player>());
-        
-        List<Player> players = team.getPlayers();
-        
-        
-        players.add(player1);
-        players.add(player2);
-        
-        team.setPlayers(players);
-        players = playerDAO.findPlayersByTeam(team);
-        Assert.assertTrue(players.contains(player1));
-        Assert.assertTrue(players.contains(player2));
-        
+        Assert.assertTrue(allPlayers.contains(player1));
+        Assert.assertTrue(allPlayers.contains(player2));
         
     }
     
+       
     @Test
     public void getPlayerByScoredGoalTest()
     {
